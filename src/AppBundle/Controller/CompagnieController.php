cf6a6c0a71e698d23699cfaeaef26a00a59308e2
Benoit <bturbe@example.com>
2015-11-22 19:48:31 +0100

    Ajout des la modification des fichiers compagnie

--- src/AppBundle/Controller/CompagnieController.php
+++ src/AppBundle/Controller/CompagnieController.php
@@ -83,4 +83,41 @@ class CompagnieController extends AController
         }
         return new JsonResponse(array('succes' => "0", 'error' => '1'));
     }
+
+    public function detailFichierAction(Request $request){
+        if ($request->isMethod ( 'POST' )) {
+            $compagnieService = $this->get('compagnie_service');
+
+            $fichier = $compagnieService->getFichierCompagnieById($request->request->get('fichier_id'));
+
+            if (!is_null($fichier) && $fichier instanceof Ressource){
+                $result['nom'] = $fichier->getTitre();
+                $result['description'] = $fichier->getDescription();
+                $result['url'] = $fichier->getUrl();
+                return new JsonResponse($result);
+            }
+        }
+        return new JsonResponse(array('succes' => "0", 'error' => '1'));
+    }
+
+
+    public function modifierFichierAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        if ($request->isMethod ( 'POST' )) {
+            $compagnieService = $this->get('compagnie_service');
+
+            $fichier = $compagnieService->getFichierCompagnieById($request->request->get('fichier_id'));
+
+            if (!is_null($fichier) && $fichier instanceof Ressource){
+                $fichier->setTitre($request->request->get('nom_fichier'));
+                $fichier->setDescription($request->request->get('description_fichier'));
+                $em->persist($fichier);
+                $em->flush();
+
+                $this->addFlash('success', 'Les informations sur le fichier ont été modifiées.');
+                return new JsonResponse(array('succes' => "1", 'error' => '0'));
+            }
+        }
+        return new JsonResponse(array('succes' => "0", 'error' => '1'));
+    }
 }
--- web/js/module_compagnie.js
+++ web/js/module_compagnie.js
@@ -66,4 +66,53 @@ $(document).ready(function() {
 
         $('.popup-confirm-suppression-fichier').modal('hide');
     });
+
+    // Modification des fichiers de la compagnie
+    $('.btn-modification-fichier').click(function() {
+        var idFichier = $(this).attr('id');
+        var url = Routing.generate('yeu_voyage_compagnie_fichier_detail');
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: {
+                fichier_id: idFichier
+            },
+            success:function(data) {
+                $(".popup-modification-fichier #update-fichier-nom").val(data.nom);
+                $(".popup-modification-fichier #update-fichier-description").val(data.description);
+                $(".popup-modification-fichier #update-fichier-url").attr('href', 'http://' + data.url);
+                $(".popup-modification-fichier #ressource-id").val(idFichier);
+            }
+        });
+
+        $('.popup-modification-fichier').modal('show');
+    });
+
+    // Sauvegarde de la modification des fichiers
+    $('.popup-modification-fichier .btn-save-popup').click(function() {
+        var idFichier = $('.popup-modification-fichier #ressource-id').val();
+        var nomFichier = $('.popup-modification-fichier #update-fichier-nom').val();
+        var descriptionFichier = $('.popup-modification-fichier #update-fichier-description').val();
+        var url = Routing.generate('yeu_voyage_compagnie_fichier_modifier');
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: {
+                fichier_id: idFichier,
+                nom_fichier: nomFichier,
+                description_fichier: descriptionFichier
+            },
+            success:function(data) {
+                if(data.succes == 1){
+                    location.reload(true);
+                } else {
+                    alert('Erreur lors de la mise à jour.')
+                }
+            }
+        });
+
+        $('.popup-modification-fichier').modal('hide');
+    });
 });
